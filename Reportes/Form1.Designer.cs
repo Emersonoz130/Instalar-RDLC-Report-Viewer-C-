d9c5b6387f42e5655ed1a67b4d562cc4556d4723
Your Name <you@example.com>
2025-06-25 11:42:56 -0600

    Proyecto con reporte basico

---
+++ Reportes/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Reportes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.miniTiendaDataSet = new Reportes.MiniTiendaDataSet();
+            this.miniTiendaDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productosTableAdapter = new Reportes.MiniTiendaDataSetTableAdapters.ProductosTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.miniTiendaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.miniTiendaDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.productosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Reportes.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // miniTiendaDataSet
+            // 
+            this.miniTiendaDataSet.DataSetName = "MiniTiendaDataSet";
+            this.miniTiendaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // miniTiendaDataSetBindingSource
+            // 
+            this.miniTiendaDataSetBindingSource.DataSource = this.miniTiendaDataSet;
+            this.miniTiendaDataSetBindingSource.Position = 0;
+            // 
+            // productosBindingSource
+            // 
+            this.productosBindingSource.DataMember = "Productos";
+            this.productosBindingSource.DataSource = this.miniTiendaDataSetBindingSource;
+            // 
+            // productosTableAdapter
+            // 
+            this.productosTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.miniTiendaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.miniTiendaDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private MiniTiendaDataSet miniTiendaDataSet;
+        private System.Windows.Forms.BindingSource miniTiendaDataSetBindingSource;
+        private System.Windows.Forms.BindingSource productosBindingSource;
+        private MiniTiendaDataSetTableAdapters.ProductosTableAdapter productosTableAdapter;
+    }
+}
+
